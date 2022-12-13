e565dfb317bf1f3a8d0f6022b9bde60c298d9319
Mahfoodha <120453689+Mahfoodha@users.noreply.github.com>
2022-12-13 13:06:05 +0400

    Add files via upload

---
+++ CategoriesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Products.Models;
+using Products.Repository;
+
+namespace Products.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriesController
+	{
+
+        CategoryRepository repository;
+
+        public CategoriesController(ILogger<WeatherForecastController> logger)
+        {
+            repository = new CategoryRepository();
+        }
+
+        [HttpGet(Name = "GetAllCategory")]
+        public ActionResult<List<Category>> Get()
+        {
+            return repository.getAll();
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult<Category> Get(int id)
+        {
+            return repository.GetCategpryById(id);
+        }
+        [HttpPost()]
+        public IResult insert(Category category)
+        {
+            bool isSuccess = repository.Insert(category);
+            return isSuccess ? Results.Ok() : Results.BadRequest();
+        }
+
+        [HttpPut()]
+        public IResult update(Category category)
+        {
+            bool isSuccess = repository.Update(category);
+            return isSuccess ? Results.Ok() : Results.BadRequest();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IResult Delete(int id)
+        {
+            bool isSuccess = repository.Delete(id);
+            return isSuccess ? Results.Ok() : Results.BadRequest();
+        }
+    }
+}
+
+
---
+++ ProductController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using Products.Repository;
+using Products.Models;
+
+namespace Products.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        ProductRepository repository;
+
+        public ProductController(ILogger<WeatherForecastController> logger)
+        {
+            repository = new ProductRepository();
+        }
+
+        [HttpGet(Name = "GetAllProducts")]
+        public ActionResult<List<Product>> Get()
+        {
+            return repository.getAll();
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult<Product> Get(int id)
+        {
+            return repository.GetProductById(id);
+        }
+        [HttpPost()]
+        public IResult insert(Product product)
+        {
+            bool isSuccess = repository.Insert(product);
+            return isSuccess  ? Results.Ok() : Results.BadRequest();
+        }
+
+        [HttpPut()]
+        public IResult update(Product product)
+        {
+            bool isSuccess = repository.Update(product);
+            return isSuccess ? Results.Ok() : Results.BadRequest();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IResult Delete(int id)
+        {
+            bool isSuccess = repository.Delete(id);
+            return isSuccess ? Results.Ok() : Results.BadRequest();
+        }
+    }
+}
+
